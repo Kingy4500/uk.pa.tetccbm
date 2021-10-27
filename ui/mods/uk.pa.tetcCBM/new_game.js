09f5fc71f30ef933156f39357c9fe1963a91372a
Kingy4500 <thekingeffect@hotmail.co.uk>
2021-10-27 13:16:00 +0100

    Add files via upload

---
+++ ui/mods/uk.pa.tetcCBM/new_game.js
@@ -0,0 +1,7 @@
+!function(){
+
+	var chat_message = "The Commander has better firepower and better buildspeed, you can build repair towers, sields, walls, land mines, laser turrets and artillery + basic resource structures. Best played FFA on small maps with a good amount of metal.";
+
+	model.localChatMessage('The Com Box Mod is Active',chat_message);
+
+}()
